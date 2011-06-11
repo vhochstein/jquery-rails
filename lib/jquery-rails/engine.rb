b9641f4a976949ca1494d6ab1d793550b6296a79
vhochstein <v.hochstein@highstone.de>
2011-06-11 19:45:39 +0200

    update lib files according to master

--- lib/jquery-rails/engine.rb
+++ lib/jquery-rails/engine.rb
@@ -4,7 +4,7 @@ module Jquery
 
     class Engine < ::Rails::Engine
       config.before_configuration do
-        require "jquery/assert_select" if ::Rails.env.test?
+        require "jquery-rails/assert_select" if ::Rails.env.test?
       end
     end
 
