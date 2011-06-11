25a59701ddb5c76d31888e1d6fb338a892f972e8
vhochstein <v.hochstein@highstone.de>
2011-06-11 19:46:50 +0200

    update jquery-rails_vho.rb according to parent repository

--- lib/jquery-rails_vho.rb
+++ lib/jquery-rails_vho.rb
@@ -1,1 +1,12 @@
-require 'jquery/rails'
+module Jquery
+  PROTOTYPE_JS = %w{prototype effects dragdrop controls}
+
+  module Rails
+    if ::Rails.version < "3.1"
+      require 'jquery-rails/railtie'
+    else
+      require 'jquery-rails/engine'
+    end
+  end
+
+end
